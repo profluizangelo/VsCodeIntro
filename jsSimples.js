5ed315f5d9bb4ad33cedca43a9826a9ce82da765
Luiz Angelo de Oliveira <profluiz.angelo@gmail.com>
2022-05-16 10:19:20 -0300

    javascript com VsCode: usando classes e bootstrap
    Básico de javascript com uso de VsCode (interface gráfica), trabalhando com classes e importação de bibliotecas (com bootstrap) e integração com github

---
+++ jsSimples.js
@@ -0,0 +1,5 @@
+console.log("-----------------------------");
+console.log("Criar e executar");
+console.log("Teste simples de execução Javascript");
+console.log("-----------------------------");
+/* git repository origin https://github.com/profluizangelo/VsCodeIntro.git */
